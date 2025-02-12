4766502597e2929b8ed723d7d00436bc6408e272
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-02-12 11:59:54 -0500

    1st commit on app.Java
    Importedd the required stuff for GUI
    
    created the windows, buttons, and click counter

--- java-ClickCounter/src/App.java
+++ java-ClickCounter/src/App.java
@@ -1,5 +1,54 @@
+//packages
+
+
+
+//imports
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+
+//main class - app
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+    public App() {
+
+        JFrame frame = new JFrame();
+
+        JButton button = new JButton("Click me");
+        button.addActionListener(this);
+
+        JLabel label = new JLabel("Number of clicks: 0");
+
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 10));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Our GUI");
+        frame.pack();
+        frame.setVisible(true);
+        
+
     }
+
+
+    //main method
+    public static void main(String[] args) throws Exception {
+    
+
+        
+    }
+
+
 }
